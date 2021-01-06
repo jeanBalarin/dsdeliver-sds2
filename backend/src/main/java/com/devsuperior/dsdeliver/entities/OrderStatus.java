477e1a761dabd8420320f615298c318d8a33c71b
Jean Marcos Francisco <jfrancisco@alunos.utfpr.edu.br>
2021-01-05 22:42:01 -0300

    Domain model

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsdeliver.entities;
+
+public enum OrderStatus {
+	
+/* a definição dos status funciona como um vetor começando na posição 0.*/	
+	
+	PENDING, DELIVERED;
+
+}
