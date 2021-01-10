f6bd4309fd3b1694324be37eddc872d9bef204b9
Jean Marcos Francisco <jfrancisco@alunos.utfpr.edu.br>
2021-01-10 19:10:20 -0300

    Fix total

--- backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -21,12 +21,12 @@ public class OrderDTO implements Serializable {
 	private Double longitude;
 	private Instant moment;
 	private OrderStatus status;
-	
+	private Double total;
 	private List<ProductDTO> products = new ArrayList<>();
 	
 	public OrderDTO() {}
 
-	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+	public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status, Double total) {
 		
 		this.id = id;
 		this.address = address;
@@ -34,6 +34,7 @@ public class OrderDTO implements Serializable {
 		this.longitude = longitude;
 		this.moment = moment;
 		this.status = status;
+		this.total = total;
 	}
 	
 	public OrderDTO(Order entity) {
@@ -44,6 +45,7 @@ public class OrderDTO implements Serializable {
 		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
+		total = entity.getTotal();
 		//aqui é recebido a lista de produtos do pedido e são convertidos para DTO.
 		products = entity.getProducts().stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
 	}
@@ -98,6 +100,14 @@ public class OrderDTO implements Serializable {
 
 	public List<ProductDTO> getProducts() {
 		return products;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
 	}
 
 
